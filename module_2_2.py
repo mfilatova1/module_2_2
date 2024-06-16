06652c79cb71b1a327366da5306875e496eb15f5
mfilatova1 <filatovam435@gmail.com>
2024-06-16 14:17:17 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,22 @@
+first = 123
+second = 456
+third = 789
+if first == second == third:
+    print(3)
+if first == second != third or first == third != second:
+    print(2)
+if first != second and first != third and second != third:
+    print(0)
+
+first = 42
+second = 69
+third = 42
+if first == second == third:
+    print(3)
+if first == second != third or first == third != second:
+    print(2)
+if first != second and first != third and second != third:
+        print(0)
+
+
+
